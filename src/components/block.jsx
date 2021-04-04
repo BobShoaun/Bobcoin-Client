2a436ed0fd5fbe1cf720b0f95b2d0f7b3f16c2bc
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-04-04 08:32:35 +0800

    init

---
+++ src/components/block.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Block extends Component {
+  state = {
+    hash: '312312321231',
+    previousHash: "",
+    nonce: "",
+    timestamp: new Date()
+  };
+	render() {
+		return (
+			<div>
+        
+				<h1>Hash</h1>
+        <p>{this.state.hash}</p>
+        <h1>Hash of previous block</h1>
+        <p>{this.state.previousHash}</p>
+				<button>nice</button>
+			</div>
+		);
+	}
+}
+
+export default Block;
