bf910d569db5f1027fdf03e2e2a530a249cfe9cb
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-05-16 01:03:55 +0800

    qr code gen

--- src/pages/GenerateKeyPage.jsx
+++ src/pages/GenerateKeyPage.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { generateKeyPair, getKeyPair } from "blockchain-crypto";
+import QRCode from "qrcode";
 
 const GenerateKeyPage = () => {
 	const [secretKey, setSecretKey] = useState("");
 	const [publicKey, setPublicKey] = useState("");
+	const [skQR, setSKQR] = useState("");
+	const [pkQR, setPKQR] = useState("");
+	const canvas = useRef(null);
 
 	const generate = () => {
 		const { sk, pk } = generateKeyPair();
 		setSecretKey(sk);
 		setPublicKey(pk);
+		generateQRCode(sk, pk);
 	};
 
 	const handleChangePrivateKey = privateKey => {
@@ -17,6 +22,7 @@ const GenerateKeyPage = () => {
 			const { sk, pk } = getKeyPair(privateKey);
 			setSecretKey(sk);
 			setPublicKey(pk);
+			generateQRCode(sk, pk);
 		} catch {
 			setSecretKey("");
 			setPublicKey("");
@@ -28,45 +34,71 @@ const GenerateKeyPage = () => {
 		localStorage.setItem("pk", publicKey);
 	};
 
+	const generateQRCode = async (sk, pk) => {
+		try {
+			setSKQR(await QRCode.toString(sk));
+			setPKQR(await QRCode.toString(pk));
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	return (
 		<section className="section">
 			<h1 className="title is-2">Generate Key</h1>
-			<div className="field">
-				<label className="label">Private / Secret key</label>
-				<div className="field has-addons mb-0">
-					<div className="control is-expanded">
-						<input
-							className="input"
-							type="text"
-							placeholder="Enter or auto-generate private key"
-							value={secretKey}
-							onChange={({ target }) => handleChangePrivateKey(target.value)}
-						></input>
-					</div>
-					<p className="control">
-						<Link className="button is-light">
-							<i className="material-icons md-18">content_copy</i>
-						</Link>
-					</p>
-				</div>
-				<p className="help is-danger">
-					As the name suggest, this key should be kept private and stored in a safe place.
-				</p>
-			</div>
 
-			<div className="field">
-				<label className="label">Public key / Address</label>
-				<div className="field has-addons mb-0">
-					<div className="control is-expanded">
-						<input className="input" type="text" value={publicKey} readOnly />
+			<div className="is-flex mb-5">
+				<section className=" mr-6" style={{ width: "50%" }}>
+					<p
+						dangerouslySetInnerHTML={{ __html: skQR }}
+						className="ml-auto mx-auto"
+						style={{ width: "300px", height: "300px" }}
+					></p>
+					<div className="field">
+						<label className="label">Private / Secret key</label>
+						<div className="field has-addons mb-0">
+							<div className="control is-expanded">
+								<input
+									className="input"
+									type="text"
+									placeholder="Enter or auto-generate private key"
+									value={secretKey}
+									onChange={({ target }) => handleChangePrivateKey(target.value)}
+								></input>
+							</div>
+							<p className="control">
+								<Link className="button is-light">
+									<i className="material-icons md-18">content_copy</i>
+								</Link>
+							</p>
+						</div>
+						<p className="help is-danger">
+							As the name suggest, this key should be kept private and stored in a safe place.
+						</p>
 					</div>
-					<p className="control">
-						<Link className="button is-light">
-							<i className="material-icons md-18">content_copy</i>
-						</Link>
-					</p>
-				</div>
-				<p className="help">This is used as an address to send and receive BBC.</p>
+				</section>
+
+				<section className="" style={{ width: "50%" }}>
+					<p
+						dangerouslySetInnerHTML={{ __html: pkQR }}
+						className="ml-auto mx-auto"
+						style={{ width: "300px", height: "300px" }}
+					></p>
+					<div className="field">
+						<label className="label">Public key / Address</label>
+						<div className="field has-addons mb-0">
+							<div className="control is-expanded">
+								<input className="input" type="text" value={publicKey} readOnly />
+							</div>
+							<p className="control">
+								<Link className="button is-light">
+									<i className="material-icons md-18">content_copy</i>
+								</Link>
+							</p>
+						</div>
+						<p className="help">This is used as an address to send and receive BBC.</p>
+					</div>
+				</section>
 			</div>
 
 			<div className="buttons is-pulled-right">
