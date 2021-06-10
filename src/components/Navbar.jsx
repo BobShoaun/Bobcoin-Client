822db630c0f78a89fa5786ab2294a522563ca55d
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-06-10 13:05:04 +0800

    nicer icons and dropdown menu for settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => (
-	<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-		<div className="navbar-brand">
-			<Link className="navbar-item mr-2" to="/">
-				<span className="material-icons-outlined mr-2">monetization_on</span>
-				<h1 className="has-text-weight-semibold">BobCoin (XBC)</h1>
-				{/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" /> */}
-			</Link>
+import "./navbar.css";
 
-			<Link
-				to="/"
-				role="button"
-				className="navbar-burger"
-				aria-label="menu"
-				aria-expanded="false"
-				data-target="navbarBasicExample"
-			>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-			</Link>
-		</div>
+const Navbar = () => {
+	const [dropdown, setDropdown] = useState(false);
+	const testNet = false;
 
-		<div id="navbarBasicExample" className="navbar-menu">
-			<div className="navbar-start">
-				<Link to="/overview" className="navbar-item has-text-weight-semi-bold mr-2">
-					Overview
+	return (
+		<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+			<div className="navbar-brand">
+				<Link className="navbar-item mr-2" to="/">
+					<span className="material-icons-outlined mr-2">monetization_on</span>
+					<h1 className="has-text-weight-semibold">BobCoin (XBC)</h1>
+					{/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" /> */}
 				</Link>
 
-				<Link to="/blockchain" className="navbar-item mr-2">
-					Blockchain
-				</Link>
-				<Link to="/new-transaction" className="navbar-item mr-2">
-					Create Transaction
-				</Link>
-				<Link to="/mine" className="navbar-item mr-2">
-					Mine
+				<Link
+					to="/"
+					role="button"
+					className="navbar-burger"
+					aria-label="menu"
+					aria-expanded="false"
+					data-target="navbarBasicExample"
+				>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
 				</Link>
 			</div>
 
-			<div className="navbar-end">
-				<div className="navbar-item">
-					<div className="buttons">
-						<Link to="/generate-key" className="button is-primary mr-3">
-							<span className="material-icons mr-2">vpn_key</span>
-							<p className="has-text-weight-semibold">Generate Key</p>
-						</Link>
+			<div id="navbarBasicExample" className="navbar-menu">
+				<div className="navbar-start">
+					<Link to="/overview" className="navbar-item has-text-weight-semi-bold mr-2">
+						Overview
+					</Link>
 
-						<Link
-							to={`/address/${localStorage.getItem("add")}`}
-							className="button has-text-weight-bold"
-						>
-							<span className="material-icons mr-2">account_balance_wallet</span>
-							<span>My Wallet</span>
-						</Link>
+					<Link to="/blockchain" className="navbar-item mr-2">
+						Blockchain
+					</Link>
+					<Link to="/new-transaction" className="navbar-item mr-2">
+						Create Transaction
+					</Link>
+					<Link to="/mine" className="navbar-item mr-2">
+						Mine
+					</Link>
+				</div>
 
-						<Link to={`/settings`} className="button is-dark has-text-weight-bold">
-							<span className="material-icons-outlined">settings</span>
-						</Link>
+				<div className="navbar-end">
+					<div className="navbar-item">
+						<div className="buttons">
+							<Link to="/generate-key" className="button is-primary mr-3">
+								<span className="material-icons-two-tone mr-2">vpn_key</span>
+								<p className="has-text-weight-bold has-text-dark">Generate Key</p>
+							</Link>
+
+							<Link
+								to={`/address/${localStorage.getItem("add")}`}
+								className="button has-text-weight-bold mr-3"
+							>
+								<span className="material-icons-two-tone mr-2">account_balance_wallet</span>
+								<span>My Wallet</span>
+							</Link>
+
+							<div class={`dropdown is-hoverable is-right ${dropdown ? "is-active" : ""}`}>
+								<div class="dropdown-trigger">
+									<button
+										aria-haspopup="true"
+										aria-controls="dropdown-menu6"
+										// onClick={() => setDropdown(d => !d)}
+										className="button is-dark px-2"
+									>
+										<span className="material-icons md-28">
+											{testNet ? "bug_report" : "language"}
+										</span>
+										<div className="indicator has-background-success"></div>
+									</button>
+								</div>
+								<div class="dropdown-menu" id="dropdown-menu6" role="menu">
+									<div class="dropdown-content fhas-text-right">
+										<div class="dropdown-item">
+											<p>
+												Currently connected to the <strong>MainNet</strong>
+											</p>
+										</div>
+										<hr class="dropdown-divider"></hr>
+										<Link to="/settings" class="dropdown-item">
+											Settings
+										</Link>
+										<a href="#" class="dropdown-item">
+											Wallets
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</nav>
-);
+		</nav>
+	);
+};
 
 export default Navbar;
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -8,18 +8,19 @@ const LandingPage = () => {
 		<section className="section">
 			<div className="has-text-centered" style={{ margin: "6em 0" }}>
 				<h1 className="title is-1 is-spaced mb-4">Welcome to Bobcoin</h1>
-				<p className="subtitle is-4 is-spaced mb-6">
-					an open source, decentralized, peer to peer, blockchain, proof of work, digital currency.
+				<p className="subtitle is-4 is-spaced mb-6 mx-auto" style={{ maxWidth: "35em" }}>
+					an open source, decentralized, peer to peer, blockchain, proof of work, digital currency
+					that will take us to Mars.
 				</p>
 				<a
 					href="https://github.com/BobShoaun/Blockchain-Crypto"
 					target="_blank"
-					className="button mr-3"
+					className="button mr-3 has-text-weight-semibold"
 				>
 					<span className="material-icons-outlined mr-2">code</span>
 					Source
 				</a>
-				<Link to="overview" className="button is-primary">
+				<Link to="overview" className="button is-primary has-text-weight-semibold">
 					<span className="material-icons-outlined mr-2">explore</span>
 					Explore
 				</Link>
@@ -39,31 +40,31 @@ const LandingPage = () => {
 				</h3>
 				<div className="is-flex is-justify-content-space-around" style={{ marginBottom: "5em" }}>
 					<div className="-has-background-info has-text-white" style={{ flex: "1 0 1" }}>
-						<span className="material-icons-outlined md-48 has-text-info">south</span>
+						<span className="material-icons-two-tone md-48 has-text-info">south</span>
 						<h2 className="title is-4 is-spaced mb-2">Low fees</h2>
 						<p className="subtitle is-6">Send money globally at the cost of peanuts</p>
 					</div>
 
 					<div className="-has-background-info has-text-white" style={{ flex: "1 0 1" }}>
-						<span className="material-icons-outlined md-48 has-text-info">emoji_emotions</span>
+						<span className="material-icons-two-tone md-48 has-text-info">emoji_emotions</span>
 						<h2 className="title is-4 is-spaced mb-2">Simple</h2>
 						<p className="subtitle is-6">Easy to use, all you need is this client</p>
 					</div>
 					<div className="-has-background-info has-text-white" style={{ flex: "1 0 1" }}>
-						<span className="material-icons-outlined md-48 has-text-info">terrain</span>
+						<span className="material-icons-two-tone md-48 has-text-info">terrain</span>
 						<h2 className="title is-4 is-spaced mb-2">High Market Cap</h2>
 						<p className="subtitle is-6">Up to 819,200,000 XBC available</p>
 					</div>
 				</div>
 				<div className="is-flex is-justify-content-space-evenly mb-6">
 					<div className="-has-background-info has-text-white" style={{ flex: "1 0 1" }}>
-						<span className="material-icons-outlined md-48 has-text-info">trending_up</span>
+						<span className="material-icons-two-tone md-48 has-text-info">trending_up</span>
 						<h2 className="title is-4 is-spaced mb-2">Big Rewards</h2>
 						<p className="subtitle is-6">Get 4096 XBC for each block mined</p>
 					</div>
 
 					<div className="-has-background-info has-text-white" style={{ flex: "1 0 1" }}>
-						<span className="material-icons-outlined md-48 has-text-info">monetization_on</span>
+						<span className="material-icons-two-tone md-48 has-text-info">monetization_on</span>
 						<h2 className="title is-4 is-spaced mb-2">Inspired by Bitcoin</h2>
 						<p className="subtitle is-6">Implemented following bitcoin's algorithms</p>
 					</div>
@@ -71,7 +72,7 @@ const LandingPage = () => {
 			</div>
 
 			<div className="has-text-centered mb-6">
-				<h1 className="title is-3">What are you waiting for?</h1>
+				<h1 className="title is-3 mb-3 is-spaced">What are you waiting for?</h1>
 				<p className="subtitle is-4">Start investing in bobcoins</p>
 				<hr />
 			</div>
@@ -80,36 +81,36 @@ const LandingPage = () => {
 					Generate a private key and address
 					<Link
 						to="generate-key"
-						className="button is-small is-info is-pulled-right has-text-weight-semibold"
+						className="button is-small is-info is-pulled-right has-text-weight-semibold px-4"
 					>
-						Go<span className="material-icons-outlined md-18 ml-2">arrow_forward</span>
+						<span className="material-icons-outlined md-28">arrow_right_alt</span>
 					</Link>
 				</li>
 				<li className="mb-5">
 					Obtain XBC by mining, or ASK the creator himself for some coins.
 					<Link
 						to="mine"
-						className="button is-small is-info is-pulled-right has-text-weight-semibold"
+						className="button is-small is-info is-pulled-right has-text-weight-semibold px-4"
 					>
-						Go<span className="material-icons-outlined md-18 ml-2">arrow_forward</span>
+						<span className="material-icons-outlined md-28">arrow_right_alt</span>
 					</Link>
 				</li>
 				<li className="mb-5">
 					Check your wallet for balance so you don't go broke!
 					<Link
 						to="wallet"
-						className="button is-small is-info is-pulled-right has-text-weight-semibold"
+						className="button is-small is-info is-pulled-right has-text-weight-semibold px-4"
 					>
-						Go<span className="material-icons-outlined md-18 ml-2">arrow_forward</span>
+						<span className="material-icons-outlined md-28">arrow_right_alt</span>
 					</Link>
 				</li>
 				<li className="mb-5">
 					Make global transactions to anyone
 					<Link
 						to="new-transaction"
-						className="button is-small is-info is-pulled-right has-text-weight-semibold"
+						className="button is-small is-info is-pulled-right has-text-weight-semibold px-4"
 					>
-						Go<span className="material-icons-outlined md-18 ml-2">arrow_forward</span>
+						<span className="material-icons-outlined md-28">arrow_right_alt</span>
 					</Link>
 				</li>
 				<li className="mb-5">Rinse and repeat, and become a valuable peer in the network.</li>
