47743e6f0bd95c85443c45b707afe25b00da3252
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-11-08 00:48:00 -0500

    fix bug

--- src/config.js
+++ src/config.js
@@ -1,20 +1,20 @@
 const {
-	NODE_ENV,
-	REACT_APP_BOBCOIN_NODE_MAIN,
-	REACT_APP_BOBCOIN_NODE_TEST,
-	REACT_APP_BOBCOIN_NODE_LOCAL_MAIN,
-	REACT_APP_BOBCOIN_NODE_LOCAL_TEST,
+  NODE_ENV,
+  REACT_APP_BOBCOIN_NODE_MAIN,
+  REACT_APP_BOBCOIN_NODE_TEST,
+  REACT_APP_BOBCOIN_NODE_LOCAL_MAIN,
+  REACT_APP_BOBCOIN_NODE_LOCAL_TEST,
 } = process.env;
 
 const development = NODE_ENV === "development";
-const useProdNodeInDev = false;
+const useProdNodeInDev = true;
 
 export const bobcoinMainnet =
-	development && !useProdNodeInDev
-		? REACT_APP_BOBCOIN_NODE_LOCAL_MAIN
-		: REACT_APP_BOBCOIN_NODE_MAIN;
+  development && !useProdNodeInDev
+    ? REACT_APP_BOBCOIN_NODE_LOCAL_MAIN
+    : REACT_APP_BOBCOIN_NODE_MAIN;
 
 export const bobcoinTestnet =
-	development && !useProdNodeInDev
-		? REACT_APP_BOBCOIN_NODE_LOCAL_TEST
-		: REACT_APP_BOBCOIN_NODE_TEST;
+  development && !useProdNodeInDev
+    ? REACT_APP_BOBCOIN_NODE_LOCAL_TEST
+    : REACT_APP_BOBCOIN_NODE_TEST;
--- src/pages/MinePage/index.jsx
+++ src/pages/MinePage/index.jsx
@@ -20,260 +20,291 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const MinePage = () => {
-	const [paramsLoading, params] = useParams();
-	const [headBlockLoading, headBlock] = useHeadBlock();
+  const [paramsLoading, params] = useParams();
+  const [headBlockLoading, headBlock] = useHeadBlock();
 
-	const keys = useSelector(state => state.wallet.keys);
-	const { externalKeys } = useSelector(state => state.wallet);
-	const api = useSelector(state => state.network.api);
+  const keys = useSelector((state) => state.wallet.keys);
+  const { externalKeys } = useSelector((state) => state.wallet);
+  const api = useSelector((state) => state.network.api);
 
-	const [miner, setMiner] = useState(
-		externalKeys[externalKeys.length - 1].addr ?? keys.address ?? ""
-	);
-	const [terminalLog, setTerminalLog] = useState([]);
-	const [successModal, setSuccessModal] = useState(false);
-	const [errorModal, setErrorModal] = useState(false);
-	const [error, setError] = useState({});
-	const [selectedTxs, setSelectedTxs] = useState([]);
-	const activeWorker = useRef(null);
+  const [miner, setMiner] = useState(
+    externalKeys[externalKeys.length - 1]?.addr ?? keys.address ?? ""
+  );
+  const [terminalLog, setTerminalLog] = useState([]);
+  const [successModal, setSuccessModal] = useState(false);
+  const [errorModal, setErrorModal] = useState(false);
+  const [error, setError] = useState({});
+  const [selectedTxs, setSelectedTxs] = useState([]);
+  const activeWorker = useRef(null);
 
-	useEffect(() => {
-		return () => {
-			// terminate worker when leaving page / component
-			console.log("terminating worker", activeWorker.current);
-			activeWorker.current?.terminate();
-		};
-	}, []);
+  useEffect(() => {
+    return () => {
+      // terminate worker when leaving page / component
+      console.log("terminating worker", activeWorker.current);
+      activeWorker.current?.terminate();
+    };
+  }, []);
 
-	const loading = headBlockLoading || paramsLoading || !headBlock;
+  const loading = headBlockLoading || paramsLoading || !headBlock;
 
-	if (loading)
-		return (
-			<div style={{ height: "70vh" }}>
-				<Loading />
-			</div>
-		);
+  if (loading)
+    return (
+      <div style={{ height: "70vh" }}>
+        <Loading />
+      </div>
+    );
 
-	const startMining = async () => {
-		if (activeWorker.current) {
-			setTerminalLog(log => [
-				...log,
-				`\nAnother mining process is currently running, to terminate it type 'mine stop'.`,
-			]);
-			return;
-		}
+  const startMining = async () => {
+    if (activeWorker.current) {
+      setTerminalLog((log) => [
+        ...log,
+        `\nAnother mining process is currently running, to terminate it type 'mine stop'.`,
+      ]);
+      return;
+    }
 
-		setTerminalLog(log => [...log, "\nForming and verifying candidate block..."]);
+    setTerminalLog((log) => [
+      ...log,
+      "\nForming and verifying candidate block...",
+    ]);
 
-		const { block, validation, target } = (
-			await axios.post(`${api}/mine/candidate-block`, {
-				previousBlock: headBlock,
-				transactions: selectedTxs,
-				miner,
-			})
-		).data;
+    const { block, validation, target } = (
+      await axios.post(`${api}/mine/candidate-block`, {
+        previousBlock: headBlock,
+        transactions: selectedTxs,
+        miner,
+      })
+    ).data;
 
-		if (validation.code !== RESULT.VALID) {
-			console.error("Candidate block is invalid, not mining: ", block);
-			setError(validation);
-			setErrorModal(true);
-			return;
-		}
+    if (validation.code !== RESULT.VALID) {
+      console.error("Candidate block is invalid, not mining: ", block);
+      setError(validation);
+      setErrorModal(true);
+      return;
+    }
 
-		setTerminalLog(log => [
-			...log,
-			`Mining started...\nprevious block: ${headBlock.hash}\ntarget hash: ${target}\n `,
-		]);
+    setTerminalLog((log) => [
+      ...log,
+      `Mining started...\nprevious block: ${headBlock.hash}\ntarget hash: ${target}\n `,
+    ]);
 
-		const worker = new Miner();
-		worker.postMessage({ block, target: hexToBigInt(target) });
-		activeWorker.current = worker;
+    const worker = new Miner();
+    worker.postMessage({ block, target: hexToBigInt(target) });
+    activeWorker.current = worker;
 
-		worker.addEventListener("message", async ({ data }) => {
-			switch (data.message) {
-				case "nonce":
-					setTerminalLog(log => [...log, `nonce reached: ${data.block.nonce}`]);
-					break;
+    worker.addEventListener("message", async ({ data }) => {
+      switch (data.message) {
+        case "nonce":
+          setTerminalLog((log) => [
+            ...log,
+            `nonce reached: ${data.block.nonce}`,
+          ]);
+          break;
 
-				case "success":
-					setTerminalLog(log => [
-						...log,
-						`\nMining successful! New block mined with...\nhash: ${data.block.hash}\nnonce: ${data.block.nonce}`,
-					]);
-					activeWorker.current = null;
+        case "success":
+          setTerminalLog((log) => [
+            ...log,
+            `\nMining successful! New block mined with...\nhash: ${data.block.hash}\nnonce: ${data.block.nonce}`,
+          ]);
+          activeWorker.current = null;
 
-					const validation = (await axios.post(`${api}/block`, { block: data.block })).data;
+          const validation = (
+            await axios.post(`${api}/block`, { block: data.block })
+          ).data;
 
-					if (validation.code !== RESULT.VALID) {
-						console.error("Block is invalid", block);
-						setError(validation);
-						setErrorModal(true);
-						break;
-					}
+          if (validation.code !== RESULT.VALID) {
+            console.error("Block is invalid", block);
+            setError(validation);
+            setErrorModal(true);
+            break;
+          }
 
-					setSelectedTxs([]);
-					setSuccessModal(true);
+          setSelectedTxs([]);
+          setSuccessModal(true);
 
-					if (Notification.permission !== "denied")
-						// ask user for permission
-						await Notification.requestPermission();
+          if (Notification.permission !== "denied")
+            // ask user for permission
+            await Notification.requestPermission();
 
-					if (Notification.permission === "granted") {
-						const notification = new Notification("Bobcoins mined!", {
-							body: "You have successfully mined a block",
-							// icon:
-						});
-						notification.onclick = () => window.focus();
-					}
-					break;
-				default:
-					console.error("invalid worker case");
-			}
-		});
-	};
+          if (Notification.permission === "granted") {
+            const notification = new Notification("Bobcoins mined!", {
+              body: "You have successfully mined a block",
+              // icon:
+            });
+            notification.onclick = () => window.focus();
+          }
+          break;
+        default:
+          console.error("invalid worker case");
+      }
+    });
+  };
 
-	const stopMining = () => {
-		if (activeWorker.current) {
-			activeWorker.current.terminate();
-			activeWorker.current = null;
-			setTerminalLog(log => [...log, `\nMining operation stopped.`]);
-			return;
-		}
-		setTerminalLog(log => [...log, `\nNo mining processes running.`]);
-	};
+  const stopMining = () => {
+    if (activeWorker.current) {
+      activeWorker.current.terminate();
+      activeWorker.current = null;
+      setTerminalLog((log) => [...log, `\nMining operation stopped.`]);
+      return;
+    }
+    setTerminalLog((log) => [...log, `\nNo mining processes running.`]);
+  };
 
-	const submitCommand = event => {
-		event.preventDefault();
-		const command = event.target.command.value;
-		event.target.command.value = "";
-		switch (command) {
-			case "mine start":
-				startMining();
-				break;
-			case "mine stop":
-				stopMining();
-				break;
-			case "help":
-				setTerminalLog(log => [
-					...log,
-					`\nCrappy ${params.name} mining terminal v1.0.0-beta\n\nList of commands:\nmine start\nmine stop\nhelp`,
-				]);
-				break;
-			default:
-				setTerminalLog(log => [...log, `\nunknown command: ${command}, type 'help' for help.`]);
-		}
-	};
+  const submitCommand = (event) => {
+    event.preventDefault();
+    const command = event.target.command.value;
+    event.target.command.value = "";
+    switch (command) {
+      case "mine start":
+        startMining();
+        break;
+      case "mine stop":
+        stopMining();
+        break;
+      case "help":
+        setTerminalLog((log) => [
+          ...log,
+          `\nCrappy ${params.name} mining terminal v1.0.0-beta\n\nList of commands:\nmine start\nmine stop\nhelp`,
+        ]);
+        break;
+      default:
+        setTerminalLog((log) => [
+          ...log,
+          `\nunknown command: ${command}, type 'help' for help.`,
+        ]);
+    }
+  };
 
-	return (
-		<section className="section">
-			<h1 className="title is-size-4 is-size-2-tablet">Mining Dashboard</h1>
-			<p className="subtitle is-size-6 is-size-5-tablet">
-				Mine from the comfort of your browser! No need for unnecessary mining clients.
-			</p>
+  return (
+    <section className="section">
+      <h1 className="title is-size-4 is-size-2-tablet">Mining Dashboard</h1>
+      <p className="subtitle is-size-6 is-size-5-tablet">
+        Mine from the comfort of your browser! No need for unnecessary mining
+        clients.
+      </p>
 
-			<div className="mb-6">
-				<Blockchain showHead />
-			</div>
+      <div className="mb-6">
+        <Blockchain showHead />
+      </div>
 
-			<section className="is-flex-tablet mb-5" style={{ gap: "3em" }}>
-				<section className="terminal mb-5" style={{ flex: "1 0 10em" }}>
-					<div className="mt-auto content" style={{ width: "100%" }}>
-						{terminalLog.map((log, index) => (
-							<pre className="terminal-output" key={index}>
-								{log}
-							</pre>
-						))}
-						<div className="is-flex is-align-items-center">
-							<span className="mr-2 has-text-weight-bold">&gt;</span>
-							<form onSubmit={submitCommand} className="is-block" style={{ width: "100%" }}>
-								<input
-									className="terminal-input"
-									name="command"
-									type="text"
-									autoComplete="off"
-									style={{ width: "100%" }}
-								/>
-							</form>
-						</div>
-					</div>
-				</section>
-				<section className="mb-6">
-					<div className="field mb-4">
-						<label className="label">Miner's Address</label>
-						<div className="field has-addons mb-0">
-							<div className="control is-expanded">
-								<input
-									onChange={({ target }) => setMiner(target.value)}
-									value={miner}
-									className="input"
-									type="text"
-									placeholder="Input miner's key"
-								/>
-							</div>
-							<p className="control">
-								<button
-									onClick={async () => {
-										setMiner(await navigator.clipboard.readText());
-										toast.success("Address pasted");
-									}}
-									className="button"
-								>
-									<i className="material-icons md-18">content_paste</i>
-								</button>
-							</p>
-						</div>
-						<p className="help">The address of the miner, where to send block reward and fees.</p>
-					</div>
+      <section className="is-flex-tablet mb-5" style={{ gap: "3em" }}>
+        <section className="terminal mb-5" style={{ flex: "1 0 10em" }}>
+          <div className="mt-auto content" style={{ width: "100%" }}>
+            {terminalLog.map((log, index) => (
+              <pre className="terminal-output" key={index}>
+                {log}
+              </pre>
+            ))}
+            <div className="is-flex is-align-items-center">
+              <span className="mr-2 has-text-weight-bold">&gt;</span>
+              <form
+                onSubmit={submitCommand}
+                className="is-block"
+                style={{ width: "100%" }}
+              >
+                <input
+                  className="terminal-input"
+                  name="command"
+                  type="text"
+                  autoComplete="off"
+                  style={{ width: "100%" }}
+                />
+              </form>
+            </div>
+          </div>
+        </section>
+        <section className="mb-6">
+          <div className="field mb-4">
+            <label className="label">Miner's Address</label>
+            <div className="field has-addons mb-0">
+              <div className="control is-expanded">
+                <input
+                  onChange={({ target }) => setMiner(target.value)}
+                  value={miner}
+                  className="input"
+                  type="text"
+                  placeholder="Input miner's key"
+                />
+              </div>
+              <p className="control">
+                <button
+                  onClick={async () => {
+                    setMiner(await navigator.clipboard.readText());
+                    toast.success("Address pasted");
+                  }}
+                  className="button"
+                >
+                  <i className="material-icons md-18">content_paste</i>
+                </button>
+              </p>
+            </div>
+            <p className="help">
+              The address of the miner, where to send block reward and fees.
+            </p>
+          </div>
 
-					<div className="field mb-5">
-						<label className="label">Head block</label>
-						<input
-							value={headBlock.hash ?? "-"}
-							className="input"
-							type="text"
-							placeholder="Enter block hash"
-							readOnly
-						></input>
-						<p className="help">Previous block to mine from.</p>
-						{/* {prevBlock?.hash !== headBlock?.hash && (
+          <div className="field mb-5">
+            <label className="label">Head block</label>
+            <input
+              value={headBlock.hash ?? "-"}
+              className="input"
+              type="text"
+              placeholder="Enter block hash"
+              readOnly
+            ></input>
+            <p className="help">Previous block to mine from.</p>
+            {/* {prevBlock?.hash !== headBlock?.hash && (
 							<p className="help is-danger is-flex">
 								<span className="material-icons-outlined md-18 mr-2">warning</span>You are no longer
 								mining from the latest block.
 							</p>
 						)} */}
-					</div>
+          </div>
 
-					<button onClick={activeWorker.current ? stopMining : startMining} className="button mb-0">
-						<i className="material-icons mr-2">memory</i>
-						{activeWorker.current ? "Stop mining" : "Start mining"}
-					</button>
-				</section>
-			</section>
+          <button
+            onClick={activeWorker.current ? stopMining : startMining}
+            className="button mb-0"
+          >
+            <i className="material-icons mr-2">memory</i>
+            {activeWorker.current ? "Stop mining" : "Start mining"}
+          </button>
+        </section>
+      </section>
 
-			<div className="is-flex is-align-items-center mb-5">
-				<div>
-					<h3 className="title is-4">Mempool</h3>
-					<p className="subtitle is-6 ">Select transactions to include from the mempool.</p>
-				</div>
-				{/* <button className="button ml-auto">
+      <div className="is-flex is-align-items-center mb-5">
+        <div>
+          <h3 className="title is-4">Mempool</h3>
+          <p className="subtitle is-6 ">
+            Select transactions to include from the mempool.
+          </p>
+        </div>
+        {/* <button className="button ml-auto">
 					<span className="material-icons-outlined md-18 mr-2">refresh</span>Refresh
 				</button> */}
-			</div>
-			<MineMempool
-				addTransaction={tx => setSelectedTxs(txs => [...txs, tx])}
-				removeTransaction={tx => setSelectedTxs(txs => txs.filter(tx2 => tx2.hash !== tx.hash))}
-			/>
+      </div>
+      <MineMempool
+        addTransaction={(tx) => setSelectedTxs((txs) => [...txs, tx])}
+        removeTransaction={(tx) =>
+          setSelectedTxs((txs) => txs.filter((tx2) => tx2.hash !== tx.hash))
+        }
+      />
 
-			<MineSuccessModal
-				isOpen={successModal}
-				close={() => setSuccessModal(false)}
-				params={params}
-				blockReward={(calculateBlockReward(params, headBlock.height) / params.coin).toFixed(8)}
-			/>
+      <MineSuccessModal
+        isOpen={successModal}
+        close={() => setSuccessModal(false)}
+        params={params}
+        blockReward={(
+          calculateBlockReward(params, headBlock.height) / params.coin
+        ).toFixed(8)}
+      />
 
-			<MineFailureModal isOpen={errorModal} close={() => setErrorModal(false)} error={error} />
-		</section>
-	);
+      <MineFailureModal
+        isOpen={errorModal}
+        close={() => setErrorModal(false)}
+        error={error}
+      />
+    </section>
+  );
 };
 
 export default MinePage;
